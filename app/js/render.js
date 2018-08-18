4335c3026e85d0054ea287d8be020b442669c697
Luis Fernando <luis.city@gmail.com>
2018-08-18 13:53:50 -0300

    criado e gerenciado evendo de play/stop

--- app/js/render.js
+++ app/js/render.js
@@ -1,9 +1,21 @@
-//console.log('Eu estou na Janela do Browser')
-
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
+
+console.log('aqui é o timer', timer);
 
 let linkSobre = document.querySelector('#link-sobre');
-
 linkSobre.addEventListener('click', function(){
     ipcRenderer.send('abrir-janela-sobre');
 })
+
+let tempo =  document.querySelector('#tempo');
+let eventoPlay = ['play >>', 'stop |_|'];
+let btnTimer = document.querySelector('#play');
+btnTimer.addEventListener('click', function() {
+    console.log('botão do timer clicado', btnTimer);
+    eventoPlay.reverse();
+    btnTimer.innerHTML = eventoPlay[0];
+    timer.iniciar(tempo);
+})
+
+
