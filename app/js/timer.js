8e0bcf98d832fd681999c0286173cd7f04b7f7bf
Luis Fernando <luis.city@gmail.com>
2018-08-18 13:55:22 -0300

    adicionado modulo moment e criado modulo timer

---
+++ app/js/timer.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+let segundos;
+let timer;
+// 00:12:32
+module.exports = {
+    iniciar(el){
+        let tempo = moment.duration(el.textContent);
+        segundos = tempo.asSeconds();
+        clearInterval(timer);
+        timer = setInterval(()=>{
+            segundos++;
+            el.textContent = this.segundosParaTempo(segundos);
+        }, 1000);
+    },parar(){
+        clearInterval(timer);
+    },segundosParaTempo(segundos){
+        return moment().startOf('day').seconds(segundos).format("HH:mm:ss");
+    }
+}
